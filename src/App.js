a293fbc2478de0f741021e16f67d4d79ca9b15d3
Juliano Costa <apfjuliano@gmail.com>
2020-11-18 14:44:39 -0300

    9. React Switch/Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import GlobalStyle from './components/GlobalStyle'
 import Nav from './components/Nav';
-import Stylednav from './components/Nav'
 import AboutUs from './pages/AboutUs'
+import ContactUs from './pages/ContactUs'
+import OurWork from './pages/OurWork'
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
     console.log(React.version);
@@ -11,7 +13,22 @@ function App() {
         <div className="App">
             <GlobalStyle />
             <Nav />
-            <AboutUs />
+            <Switch>
+                <Route exact path="/" component={AboutUs} />
+                <Route path="/work" component={OurWork} />
+                <Route path="/contact" component={ContactUs} />
+                {/* 
+                <Route exact path="/" >
+                    <AboutUs />
+                </Route>
+                <Route path="/work" >
+                    <OurWork />
+                </Route>
+                <Route path="/contact" >
+                    <ContactUs />
+                </Route> 
+                */}
+            </Switch>
         </div>
     );
 }
