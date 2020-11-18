32328d2760683cd2a2771ca5cd757a6cef144110
Juliano Costa <apfjuliano@gmail.com>
2020-11-18 14:15:53 -0300

    Revert "9. React Switch/Router"
    This reverts commit 4a59d2fac536cae2b1a82bace92a6386c85f6f5a.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import GlobalStyle from './components/GlobalStyle'
 import Nav from './components/Nav';
+import Stylednav from './components/Nav'
 import AboutUs from './pages/AboutUs'
-import ContactUs from './pages/CantactUs'
-import OurWork from './pages/OurWork'
-import { Switch, Route } from 'react-router-dom'
 
 function App() {
     console.log(React.version);
@@ -13,15 +11,7 @@ function App() {
         <div className="App">
             <GlobalStyle />
             <Nav />
-            <Route exact path="/" >
-                <AboutUs />
-            </Route>
-            <Route path="/work" >
-                <OurWork />
-            </Route>
-            <Route path="/contact" >
-                <ContactUs />
-            </Route>
+            <AboutUs />
         </div>
     );
 }
--- src/pages/OurWork.js
+++
@@ -1,9 +0,0 @@
-const OurWork = () => {
-    return (
-        <div>
-            <h1>Our word</h1>  
-        </div>
-    )
-}
-
-export default OurWork
