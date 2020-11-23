9bee53df78ecea895bd265cf085b2c1e7c8a26ad
Juliano Costa <apfjuliano@gmail.com>
2020-11-23 20:48:07 -0300

    14. About Animation

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,6 +2,7 @@ import home1 from '../img/home1.png'
 import { About, Description, Image, Hide } from '../styles'
 //Frame Mption
 import { motion } from 'framer-motion'
+import { titleAnim, fade, photoAnim } from '../animation'
 
 const AboutSection = () => {
     return (
@@ -9,26 +10,26 @@ const AboutSection = () => {
             <Description>
                 <motion.div>
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             We work to make
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             your <span>dreams</span> come 
                         </motion.h2>
                     </Hide>
                     <Hide>
-                        <motion.h2>
+                        <motion.h2 variants={titleAnim}>
                             true.
                         </motion.h2>
                     </Hide>
                 </motion.div>
-                <p>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</p>
-                <button>Contact Us</button>
+                <motion.p variants={fade}>Contact us for any photography or videography ideas that you have. We have professionals with amazing skills.</motion.p>
+                <motion.button variants={fade}>Contact Us</motion.button>
             </Description>
             <Image>
-                <img src={home1} alt="Guy with a camera" />
+                <motion.img variants={photoAnim} src={home1} alt="Guy with a camera" />
             </Image>
         </About>
     )    
