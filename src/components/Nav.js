83d8da3de265c5c3a0097d315c0ce45aed94af88
Juliano Costa <apfjuliano@gmail.com>
2020-11-30 03:09:14 -0300

    22.1 Nav Animation

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { NavLink, Link } from 'react-router-dom'
+import { lineAnim } from '../animation'
 
 const Nav = () => {
     return (
@@ -7,13 +8,13 @@ const Nav = () => {
             <h1><Link id="logo" to="/">Capture</Link></h1>
             <ul>
                 <li>
-                    <NavLink exact to="/">1. About Us</NavLink>
+                    <NavLink exact to="/">About Us</NavLink>
                 </li>
                 <li>
-                    <NavLink exact to="/work">2. Our Work</NavLink>
+                    <NavLink exact to="/work">Our Work</NavLink>
                 </li>
                 <li>
-                    <NavLink exact to="/contact">3. Contact Us</NavLink>
+                    <NavLink exact to="/contact">Contact Us</NavLink>
                 </li>
             </ul>
         </StyledNav>
@@ -48,8 +49,30 @@ const StyledNav = styled.nav`
         position: relative;
     }
     .active{
-        text-decoration: underline white solid;
+        /* text-decoration: underline white solid; */
         font-weight: bold;
+    }
+    .active::before {
+        content: '';
+        position: absolute;
+        border-color: #23d997;
+        border-style: solid;
+        border-width: 0 0.2rem 0.2rem 0;
+        width: 6.5rem;
+        height: 1.6rem;
+        bottom: -0.2rem;
+        right: -0.55rem;
+        margin-top: -1em;
+        border-radius: 0 0 10px 0;
+        transform: rotate(4deg);
+        -webkit-transform: rotate(4deg);
+        transition: all 0.4s linear;
+    }
+    .active:hover::before {
+        transform: rotate(0deg);
+        -webkit-transform: rotate(0deg);
+        width: 0;
+        border-color: #D96ED4;
     }
 `
 export default Nav
