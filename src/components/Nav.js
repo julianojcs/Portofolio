a756c1071ec5a7ad3aed9194779753fd2ba231db
Juliano Costa <apfjuliano@gmail.com>
2020-12-01 18:47:09 -0300

    24. Nav Line Animation (commented)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import { NavLink, Link } from 'react-router-dom'
+// import { useLocation } from 'react-router-dom'
 // import { motion } from 'framer-motion'
 
 const Nav = () => {
